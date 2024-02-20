2389e08219bcbd34a053db30a25d7aeb4afb60d9
Sanjin Babic <sanjinbabic95@gmail.com>
2024-02-20 20:58:42 +0100

    PraksaUtorak

--- script.js
+++ script.js
@@ -7,3 +7,52 @@ function openCloseMenu() {
     }
  
 }
+
+var index = 0;
+    change();
+
+    function change() {
+
+        var x = document.getElementsByClassName('slides');
+
+        for(var i = 0; i < x.length; i++) { 
+            x[i].style.display = "none"; 
+        }
+
+        index++;
+
+        if(index > x.length) { 
+            index = 1; 
+        }
+
+        x[index - 1].style.display = "block";
+
+        setTimeout(change, 4000);
+    }
+
+    let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
